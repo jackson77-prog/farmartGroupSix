2056e4645965cf28e574ac3c6a4f9bc0a6aac10d
jackson77-prog <jackson.njihia@student.moringaschool.com>
2024-08-09 17:50:34 +0300

    changed proxy in vite.config.js to development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
+    proxy: process.env.NODE_ENV === 'development' ? {
       '/api': {
         target: 'https://farmartgroupsix-8.onrender.com/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    },
+    } : {},
   },
 });
